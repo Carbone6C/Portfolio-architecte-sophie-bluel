947066be67f454ae7a2a514e33f01bd958c966bc
Carbone6C <danny.fu91390@gmail.com>
2023-11-15 20:14:29 +0100

    Création de la fonnction de créations des filtres et de ses listeners

--- FrontEnd/assets/script.js
+++ FrontEnd/assets/script.js
@@ -1,9 +1,12 @@
-async function afficherWorks() {
-    const reponseWorks = await fetch("http://localhost:5678/api/works");
-    const works = await reponseWorks.json();
+// Déclaration de toutes les variables :
+// Tableau récupérant la liste des travaux sur le backend :
+let reponseWorks = await fetch('http://localhost:5678/api/works');
+let works = await reponseWorks.json();
+
+async function afficherWorks(works) {
 
     // Récupération de l'élément du DOM qui accueillera les images
-    const gallery = document.querySelector(".gallery")
+    const gallery = document.querySelector(".gallery");
     for (let i = 0; i < works.length; i++) {
 
         const worksObject = works[i];
@@ -11,9 +14,9 @@ async function afficherWorks() {
         const figure = document.createElement("figure");
         // Création des balises 
         const img = document.createElement("img");
-        img.src = works[i].imageUrl;
+        img.src = worksObject.imageUrl;
         const imgLegend = document.createElement("figcaption");
-        imgLegend.innerText = works[i].title;
+        imgLegend.innerText = worksObject.title;
 
         // On rattache la balise figure a la gallery
         gallery.appendChild(figure);
@@ -22,25 +25,79 @@ async function afficherWorks() {
         figure.appendChild(img);
         figure.appendChild(imgLegend)
     }
+}
 
-    const reponseCategories = await fetch("http://localhost:5678/api/categories");
-    const categories = await reponseCategories.json();
+async function afficherFilters() {
+    let reponseCategories = await fetch('http://localhost:5678/api/categories');
+    let categories = await reponseCategories.json();
+    // Création de la div du DOM qui accueillera les filtres
+    const portofoliotitle = document.querySelector("#portfolio h2");
+    const allFilter = document.createElement("div");
+    allFilter.className = "filters"; 
+    portofoliotitle.after(allFilter);
+
+     // Intégration d'un bouton affichant tous les travaux
+    const toutFilter = document.createElement("button");
+    toutFilter.className = "filterButton";
+    toutFilter.innerText = "Tous";
+    toutFilter.id = "toutFilter";
+    allFilter.appendChild(toutFilter);
+    
+
+    //Ajout du listener pour le button Tous
+
+    toutFilter.addEventListener("click", function () {
+    document.querySelector('.gallery').innerHTML = "";
+    afficherWorks(works);
+    });
+
+    // Création d'un Array pour lister les catégories
+
+    const filterCat = [];
+
+    for (let i = 0; i < works.length; i++) {
+      const work = works[i];
+      if (!filterCat.includes(work.category.name)) {
+        filterCat.push(work.category.name);
+      };
+    }
+    // Création des boutons dédiées aux filtres, attributions de classes et ID pour ces boutons
 
     for (let i = 0; i < categories.length; i++) {
+        const filter = document.createElement("button");
+        filter.className = "filterButton";
+        filter.id = `${filterCat[i]}`;
+        filter.innerText = filterCat[i];
 
-        const categoriesFilters = categories[i];
-        // Récupération de la section du DOM qui accueillera les filtres
-        const portofoliotitle = document.querySelector("#portfolio h2");
-        // Création d’une balise dédiée aux filtres
-        const input = document.createElement("input");
-        input.type = "submit";
-        input.value = categories[i].name;
+        // On rattache les boutons à la div de tous filtres
+        allFilter.appendChild(filter);
+        // Création des Listeners pour ces boutons
 
-        // On rattache la balise figure a la gallery
-        portofoliotitle.after(input);
+        filter.addEventListener("click", function () {
+        const btnId = this.id;
+        const filWork = works.filter(function (work) {
+            return work.category.name == btnId;
+        })
+
+        // on supprime le contenu HTML de la gallery pour l'actualiser avec une nouvelle variable dans notre fonction : filWork
+        document.querySelector(".gallery").innerHTML = "";
+        afficherWorks(filWork)
+        })
     }
-
 }
 
 
-afficherWorks()
+
+afficherWorks(works);
+afficherFilters(works)
+
+
+
+
+
+
+
+
+
+
+
